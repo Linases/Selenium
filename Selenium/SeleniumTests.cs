aa55d5cf948ec523d5661623342c5e763a2ee0f6
Linases <lina.tauraite@gmail.com>
2023-10-03 21:01:31 +0300

    Inprogress

--- Selenium/SeleniumTests.cs
+++ Selenium/SeleniumTests.cs
@@ -1,17 +1,37 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace Selenium
 {
     public class SeleniumTests
     {
-        [Test]
-        public void TestWebsiteTitle()
-        {
-            var driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("")
-            driver.Navigate().GoToUrl("https://www.example.com");
+        ChromeDriver driver = new ChromeDriver();
 
+        [Test]
+        public void TestWebsiteLogin()
+        {
+
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            IWebElement elementUserName = driver.FindElement(By.Id("user-name"));
+            elementUserName.SendKeys("standard_user");
+            IWebElement elementPassword = driver.FindElement(By.Id("password"));
+            elementPassword.SendKeys("secret_sauce");
+
+            IWebElement elementLoginButton = driver.FindElement(By.Id("login-button"));
+            elementLoginButton.Click();
+
+            string pageUrl = driver.Url;
+            Assert.That(pageUrl, Is.EqualTo("https://www.saucedemo.com/inventory.html"));
+
+            IWebElement elementItem = driver.FindElement(By.Id("item_4_title_link"));
+            elementItem.Click();
+
+            string itemUrl = driver.Url;
+            Assert.That(itemUrl, Is.EqualTo("https://www.saucedemo.com/inventory-item.html?id=4"));
+
+            driver.Quit();
         }
     }
 }
+
