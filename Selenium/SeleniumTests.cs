d3729030a77b759e41f662c5c7684562f70063bb
Linases <lina.tauraite@gmail.com>
2023-10-04 20:42:16 +0300

    Please review module2

--- Selenium/SeleniumTests.cs
+++ Selenium/SeleniumTests.cs
@@ -3,8 +3,10 @@ using NUnit.Framework;
 using NUnit.Framework.Internal;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Text;
 using OpenQA.Selenium.Support.UI;
 using System.ComponentModel;
+using System.Linq;
 
 namespace Selenium
 {
@@ -23,8 +25,8 @@ namespace Selenium
         {
             driver.Quit();
         }
-        [Test, Order(3)]
-        public void SuccessfulLogin()
+        [Test, Order(1)]
+        public void SuccessfullLoginAndItemOpening()
         {
             driver.Navigate().GoToUrl("https://www.saucedemo.com/");
             var elementUserName = driver.FindElement(By.Id("user-name"));
@@ -37,14 +39,25 @@ namespace Selenium
 
             string pageUrl = driver.Url;
             Assert.That(pageUrl, Is.EqualTo("https://www.saucedemo.com/inventory.html"));
+            string pageTitle = driver.Title;
+            Assert.That(pageTitle, Is.EqualTo("Swag Labs"));
 
             var elementItem = driver.FindElement(By.Id("item_4_title_link"));
             elementItem.Click();
 
             string itemUrl = driver.Url;
             Assert.That(itemUrl, Is.EqualTo("https://www.saucedemo.com/inventory-item.html?id=4"));
+
+            var itemTitle = driver.FindElement(By.CssSelector("[class*='inventory_details_name']"));
+            Assert.That(itemTitle.Text.Contains("Sauce Labs Backpack"));
+
+            var itemDescription = driver.FindElement(By.CssSelector("[class*='inventory_details_desc']"));
+            Assert.That(itemDescription.Text.Contains("streamlined Sly Pack"));
+
+            var itemPrice = driver.FindElement(By.ClassName("inventory_details_price"));
+            Assert.That(itemDescription.Text.Contains("$29.99"));
         }
-        [Test, Order(1)]
+        [Test, Order(2)]
         public void FailedLoginWithEmptyCredentials()
         {
             driver.Navigate().GoToUrl("https://www.saucedemo.com/");
@@ -54,9 +67,15 @@ namespace Selenium
             elementLoginButton.Click();
             var errorMessage = driver.FindElement(By.CssSelector("[data-test*='error']"));
             Assert.That(errorMessage.Text.Contains("Username is required"));
+           
+            var errorMarkUserName = driver.FindElement(By.CssSelector("[class*=svg-inline--fa]"));
+            Assert.That(errorMarkUserName.Displayed);
+           
+            var errorMarkPassword = driver.FindElement(By.CssSelector("[class*=svg-inline--fa]"));
+            Assert.That(errorMarkPassword.Displayed);
         }
-        [Test, Order(2)]
-        public void FailedLoginWithInvalidCredentials() 
+        [Test, Order(3)]
+        public void FailedLoginWithInvalidCredentials()
         {
             driver.Navigate().GoToUrl("https://www.saucedemo.com/");
             var elementUserName = driver.FindElement(By.Id("user-name"));
@@ -65,8 +84,15 @@ namespace Selenium
             elementPassword.SendKeys("invalid_password");
             var elementLoginButton = driver.FindElement(By.Id("login-button"));
             elementLoginButton.Click();
+            
             var errorMessage = driver.FindElement(By.CssSelector("[data-test*='error']"));
-            Assert.That(errorMessage.Text.Contains("Username and password do not match")); 
+            Assert.That(errorMessage.Text.Contains("Username and password do not match"));
+            
+            var errorMarkUserName = driver.FindElement(By.CssSelector("[class*=svg-inline--fa]"));
+            Assert.That(errorMarkUserName.Displayed);
+           
+            var errorMarkPassword = driver.FindElement(By.CssSelector("[class*=svg-inline--fa]"));
+            Assert.That(errorMarkPassword.Displayed);
         }
     }
 }
