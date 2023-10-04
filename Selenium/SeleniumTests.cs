a676e0d32e4c1cf8fadbb299782a8962137cb0f7
Linases <lina.tauraite@gmail.com>
2023-10-04 13:05:00 +0300

    ready for review

--- Selenium/SeleniumTests.cs
+++ Selenium/SeleniumTests.cs
@@ -1,36 +1,75 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.ComponentModel;
 
 namespace Selenium
-{ 
+{
     [TestFixture]
     public class SeleniumTests
     {
-        [Test]
+        private IWebDriver driver;
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+        }
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+        [Test, Order(3)]
         public void SuccessfulLogin()
         {
-            ChromeDriver driver = new ChromeDriver();
             driver.Navigate().GoToUrl("https://www.saucedemo.com/");
-            IWebElement elementUserName = driver.FindElement(By.Id("user-name"));
+            var elementUserName = driver.FindElement(By.Id("user-name"));
             elementUserName.SendKeys("standard_user");
-            IWebElement elementPassword = driver.FindElement(By.Id("password"));
+            var elementPassword = driver.FindElement(By.Id("password"));
             elementPassword.SendKeys("secret_sauce");
 
-            IWebElement elementLoginButton = driver.FindElement(By.Id("login-button"));
+            var elementLoginButton = driver.FindElement(By.Id("login-button"));
             elementLoginButton.Click();
 
             string pageUrl = driver.Url;
             Assert.That(pageUrl, Is.EqualTo("https://www.saucedemo.com/inventory.html"));
 
-            IWebElement elementItem = driver.FindElement(By.Id("item_4_title_link"));
+            var elementItem = driver.FindElement(By.Id("item_4_title_link"));
             elementItem.Click();
 
             string itemUrl = driver.Url;
             Assert.That(itemUrl, Is.EqualTo("https://www.saucedemo.com/inventory-item.html?id=4"));
-
-            driver.Quit();
+        }
+        [Test, Order(1)]
+        public void FailedLoginWithEmptyCredentials()
+        {
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            var elementUserName = driver.FindElement(By.Id("user-name"));
+            var elementPassword = driver.FindElement(By.Id("password"));
+            var elementLoginButton = driver.FindElement(By.Id("login-button"));
+            elementLoginButton.Click();
+            var errorMessage = driver.FindElement(By.CssSelector("[data-test*='error']"));
+            Assert.That(errorMessage.Text.Contains("Username is required"));
+        }
+        [Test, Order(2)]
+        public void FailedLoginWithInvalidCredentials() 
+        {
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            var elementUserName = driver.FindElement(By.Id("user-name"));
+            elementUserName.SendKeys("invalid_user");
+            var elementPassword = driver.FindElement(By.Id("password"));
+            elementPassword.SendKeys("invalid_password");
+            var elementLoginButton = driver.FindElement(By.Id("login-button"));
+            elementLoginButton.Click();
+            var errorMessage = driver.FindElement(By.CssSelector("[data-test*='error']"));
+            Assert.That(errorMessage.Text.Contains("Username and password do not match")); 
         }
     }
 }
+
+
+
 
