4ac5609233335d7c6950e4a643956bf06327dbeb
Linases <lina.tauraite@gmail.com>
2023-10-05 21:10:50 +0300

    Work in progress

--- Functionality_Tests/FunctionalityTests.cs
+++ Functionality_Tests/FunctionalityTests.cs
@@ -6,16 +6,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium.Chrome;
-
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.DevTools.V115.Page;
 
 namespace Functionality_Tests_Suit
 {
-    [TestFixture]  
-    
-    public class FunctionalityTests: BaseTest
+    [TestFixture]
+
+    public class FunctionalityTests : BaseTest
     {
- 
-        [Test, Order (1)]
+
+        [Test, Order(1)]
+
         public void PriceSorting()
         {
             SuccessfulLogin();
@@ -29,13 +32,9 @@ namespace Functionality_Tests_Suit
             var elementItemsList = driver.FindElements(By.ClassName("inventory_list"));
             Assert.That(elementItemsList, Is.Ordered.Ascending);
         }
-        [Test, Order (2)]
+    
+        [Test, Order(2)]
 
-        public void AddToCart()
-        {
-            AddProductToCart();
-        }
-        [Test, Order (3)]
         public void RemoveFromCart()
         {
             AddProductToCart();
@@ -45,6 +44,20 @@ namespace Functionality_Tests_Suit
             elementRemoveButton.Click();
             var elementShopingcartIcon2 = driver.FindElement(By.ClassName("shopping_cart_link"));
             Assert.That(elementShopingcartIcon2.Text.Contains('1'), Is.False);
+        }
+        [Test, Order(3)]
+
+        public void Logout()
+        {
+            SuccessfulLogin();
+            var elementMeniuButton = driver.FindElement(By.Id("react-burger-menu-btn"));
+            elementMeniuButton.Click();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("logout_sidebar_link"))).Click();
+            string pageUrl = driver.Url;
+            Assert.That(pageUrl, Is.EqualTo("https://www.saucedemo.com/"));
+            var elementLoginButton = driver.FindElement(By.Id("login-button"));
+            Assert.That(elementLoginButton.Displayed);
         }
     }
 }
