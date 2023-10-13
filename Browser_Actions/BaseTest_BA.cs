ba55a8f9ee9620679f86b09b2983f413d945d5bd
Linases <lina.tauraite@gmail.com>
2023-10-13 16:14:45 +0300

    new Module5-branch 1 scenario

---
+++ Browser_Actions/BaseTest_BA.cs
@@ -0,0 +1,37 @@
+﻿using Functionality_Tests_Suit.Constants;
+using Functionality_Tests_Suit.FactoryPattern;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Browser_Actions
+{
+    public class BaseTest_BA
+    {
+        protected static IWebDriver Driver;
+        protected readonly string MainUrl;
+
+        public BaseTest_BA()
+        {
+            MainUrl = "http://the-internet.herokuapp.com";
+        }
+
+        [OneTimeSetUp]
+        public static void OneTimeSetUp()
+        {
+            Driver = BrowserFactory.GetDriver(BrowserType.Chrome);
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            Driver.Navigate().GoToUrl(MainUrl);
+        }
+
+        [OneTimeTearDown]
+        public static void TearDown()
+        {
+            BrowserFactory.CloseDriver();
+        }
+
+    }
+}
---
+++ Browser_Actions/Browser_ActionTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniumExtras.WaitHelpers;
+
+namespace Browser_Actions
+{
+    [TestFixture]
+    public class Browser_ActionTests : BaseTest_BA
+    {
+        [Test]
+        public void OpenNewWindowHandleTabs()
+        {
+            string originalWindow = Driver.CurrentWindowHandle;
+            Assert.AreEqual(Driver.WindowHandles.Count, 1);
+
+            var elementMultipleWindows = Driver.FindElement(By.CssSelector("[href*='windows']"));
+            elementMultipleWindows.Click();
+          
+            var elementClickHere = Driver.FindElement(By.CssSelector("[href*='windows']"));
+            elementClickHere.Click();
+            var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(20));
+            wait.Until(wd => wd.WindowHandles.Count == 2);
+           
+            foreach (string window in Driver.WindowHandles)
+            {
+                if (originalWindow != window)
+                {
+                    Driver.SwitchTo().Window(window);
+                    break;
+                }
+            }
+            wait.Until(wd => wd.Url == $"{MainUrl}/windows/new");
+            var elementContent = Driver.FindElement(By.ClassName("example"));
+            Assert.That(elementContent.Displayed);
+        }
+    }
+}
