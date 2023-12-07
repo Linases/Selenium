c9b57576acc75b09fa47d825181acf879b3ad3d4
Linases <lina.tauraite@gmail.com>
2023-12-07 22:50:47 +0200

    aded DeleteAndEnterText method to TextBox

--- Wrappers/TextBox.cs
+++ Wrappers/TextBox.cs
@@ -1,4 +1,6 @@
 ﻿using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using Utilities;
 
 namespace Wrappers
 {
@@ -22,6 +24,20 @@ namespace Wrappers
         {
             Element.SendKeys(Keys.Control + "a");
             Element.SendKeys(Keys.Backspace);
+        }
+
+        private void DeleteAndEnterText(string text, By locator)
+        {
+            Element = WebDriverExtensions.GetWait(Driver).Until(ExpectedConditions.ElementToBeClickable(locator));
+            Click();
+            if (Element.GetAttribute("value").Length > 0)
+            {
+                DeleteAllTextWithKey();
+            }
+            if (Element.Enabled)
+            {
+                SendKeys(text);
+            }
         }
     }
 }
